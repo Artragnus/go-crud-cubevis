ced576255a7f16fcf1abd75ff9f687cfb77c248a
João Paulo <joaopptt@hotmail.com>
2024-11-29 15:03:56 -0300

    feat: create baby db schema

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,7 +1,17 @@
 CREATE TABLE users(
     id CHAR(32) PRIMARY KEY UNIQUE NOT NULL,
-    name VARCHAR(50),
-    email VARCHAR(50) NOT NULL,
-    password VARCHAR(50) NOT NULL,
-)
+    name VARCHAR(255),
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE address (
+    id CHAR(32) PRIMARY KEY UNIQUE NOT NULL,
+    user_id CHAR(32) NOT NULL REFERENCES users(id),
+    address VARCHAR(255) NOT NULL,
+    number VARCHAR(255),
+    zip_code VARCHAR(255),
+    city VARCHAR(255) NOT NULL,
+    state VARCHAR(255) NOT NULL
+);
 
