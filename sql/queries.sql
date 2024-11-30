30f5f4397fce213d56f3e63c395343bcb40df3d1
João Paulo <joaopptt@hotmail.com>
2024-11-30 12:03:46 -0300

    feat: add querie to create addresse

--- sql/queries.sql
+++ sql/queries.sql
@@ -12,3 +12,6 @@ UPDATE users set name = $2, email = $3, password = $4 WHERE id = $1;
 
 -- name: DeleteUser :exec
 DELETE FROM users WHERE id = $1;
+
+-- name: CreateAddress :exec
+INSERT INTO addresses (id, user_id, address, number, zip_code, city, state) VALUES ($1, $2, $3, $4, $5, $6, $7);
