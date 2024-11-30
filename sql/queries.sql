220b21e3c40588b49618975eb8c805b7d9763f5c
João Paulo <joaopptt@hotmail.com>
2024-11-30 08:38:59 -0300

    feat: add querie to find by email

--- sql/queries.sql
+++ sql/queries.sql
@@ -1,8 +1,12 @@
 -- name: CreateUser :exec
 INSERT INTO users (id, name, email, password) VALUES ($1, $2, $3, $4);
 
--- name: GetUser :one
+-- name: GetUserById :one
 SELECT * FROM users WHERE id = $1;
+
+-- name: GetUserByEmail :one
+
+SELECT * FROM users WHERE email = $1;
 
 -- name: UpdateUser :exec
 UPDATE users set name = $2, email = $3, password = $4 WHERE id = $1;
